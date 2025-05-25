8c771f700f23a1454a5ca74f5423ae0492d9bd4d
rakhi <rakhisingh876.com@gmail.com>
2025-05-26 04:38:31 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+var allElems = document.querySelectorAll('.elem');
+var fullelem = document.querySelectorAll('.fullelem');
 
-
+allElems.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        fullelem[elem.id].style.display = 'block';
+    });
+});
